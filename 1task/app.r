10d7e2ae5d4cd87d988d26e480d18825cb6d22a5
ostapkharysh <kharyshostap@gmail.com>
2021-01-16 18:09:04 +0200

    "Updating the map"

--- 1task/app.r
+++ 1task/app.r
@@ -12,30 +12,26 @@ cont <- read.csv(file = 'task1data.csv', sep=";",
 colnames(cont) <- c("country", "RD")
 
 
-eu <- rgdal::readOGR("custom.geo.json", encoding = "UTF-8")
+raw_eu <- rgdal::readOGR("Europe.geo.json", encoding = "UTF-8")
 
-eu$admin[eu$admin == "Macedonia"] <- "North Macedonia"
+raw_eu$admin[raw_eu$admin == "Macedonia"] <- "North Macedonia"
 cont$country[cont$country == "Czechia"] <- "Czech Republic"
-cont = head(cont[order(match(cont[,1],eu$admin)),], -5)
 
 
-length(eu)
+eu <- raw_eu
+cont <- head(cont[order(match(cont[,1],eu$admin)),], -5)
 eu <- eu[eu$admin %in% cont$country,]
-length(eu)
-RD <- cont[cont$country %in% eu$admin,]$RD
-eu$rd <- RD
+eu$rd <- cont[cont$country %in% eu$admin,]$RD
+
+no_data_eu <- subset(raw_eu,admin %in% c('Bosnia and Herzegovina','Albania','Andorra', 'Montenegro', 'Republic of Serbia', 'Belarus', 'Moldova', 'Kosovo'))
 
 
 pal <- colorNumeric("Blues", eu$rd)
-
 # Define UI for dataset viewer app ----
 ui <- fluidPage(
-  titlePanel(title=h4("What’s the distribution of Research & Development applied in business sector in the different areas of the EU", align="center")),
-  leafletOutput("map",  width = "100%", height = "500px")
-  
+  titlePanel(title=h4("What’s the distribution of Research & Development score applied in business sector in the different areas of the Europe?", align="center")),
+  leafletOutput("map",  width = "100%", height = "550px")
 )
-
-
 
 
 
@@ -44,17 +40,60 @@ server <- function(input, output) {
   
   output$map <- renderLeaflet({
     
+    labels <- sprintf(
+      "<strong>%s</strong><br/> R&D:%g <br/>",
+      eu$admin, 
+      eu$rd
+    ) %>% lapply(htmltools::HTML)
+    
+    
+    not_prov_labels <- sprintf(
+      "<strong>%s</strong><br/> R&D: No data provided <br/>",
+      no_data_eu$admin
+    ) %>% lapply(htmltools::HTML)
+    
     leaflet(eu) %>%
-      addTiles() %>%
+      addProviderTiles("MapBox", options = providerTileOptions(
+        id = "mapbox.light",
+        accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      addPolygons(data = no_data_eu, 
+                  stroke = FALSE, 
+                  smoothFactor = 0.3,
+                  fillOpacity = 1,
+                  fillColor = "#CECFCE",
+                  label = not_prov_labels
+      ) %>%
+      
+      addPolylines(data = no_data_eu,
+                  color = "black",
+                  stroke = TRUE,
+                  opacity = 1, 
+                  weight = 0.5) %>% 
+      
       addPolygons(stroke = FALSE, 
-                  smoothFactor = 0.3, fillOpacity = 1, fillColor = ~pal(RD), 
-                  label = ~paste0(admin, ": ", formatC(RD, big.mark = ","))) %>%
-      addLegend(pal = pal, values = ~RD, opacity = 1.0)
+                  smoothFactor = 0.3,
+                  fillOpacity = 1,
+                  fillColor = ~pal(rd), 
+                  label = labels, #~paste0(admin, ": ", formatC(RD, big.mark = ","))
+                  highlight = highlightOptions(
+                    weight = 3,
+                    fillOpacity = 0.5,
+                    fillColor = "#36BA51",
+                    opacity = 1.0,
+                    bringToFront = TRUE,
+                    sendToBack = TRUE)
+                  ) %>%
+      
+      # Add border
+      addPolylines(color = "black",
+                   stroke = TRUE,
+                   opacity = 1, 
+                   weight = 0.5) %>% 
+      
+      addLegend(pal = pal, values = ~rd, opacity = 1.0, title = "R&D score")
 })
   
 }
 
 # Create Shiny app ----
 shinyApp(ui, server)
-#app <- shinyApp(ui = ui, server = server)
-#runApp(app, launch.browser = TRUE)
