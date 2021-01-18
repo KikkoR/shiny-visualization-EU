c4684b934029c8a1f3e10c267b761e46b222323e
Federico <federico.rodigari@studenti.unitn.it>
2021-01-19 00:38:20 +0100

    fixed scales and names

--- 2task/app.R
+++ 2task/app.R
@@ -1,5 +1,3 @@
-#versione che funzionicchia brutte troie
-
 library(shiny)
 library(tidyverse)
 library(xlsx)
@@ -9,62 +7,72 @@ sheetIndex<-6
 df<-read.xlsx(file, sheetIndex, header=TRUE)
 df$NA..1 <- as.factor(df$NA..1) 
 df = df[-1,]
-df$location<-factor(c("w","e","e","n","w","n","w","s","s","w","e","s","s","n","n","w","e","s","w","w","e","s","e","e","e","n","n","w","n","e","e","n","e","w","s","e"))
+df$location<-factor(c("Western"," Eastern"," Eastern"," Northern","Western"," Northern","Western"," Southern"," Southern","Western"," Eastern"," Southern"," Southern"," Northern"," Northern","Western"," Eastern"," Southern","Western","Western"," Eastern"," Southern"," Eastern"," Eastern"," Eastern"," Northern"," Northern","Western"," Northern"," Eastern"," Eastern"," Northern"," Eastern","Western"," Southern"," Eastern"))
+df[ df == ":" ] <- NA
+df=na.omit(df)
+cols <- names(df)[3:30]
+df[cols] <- lapply(df[cols], as.numeric)
+names(df)[1] <- "nation"
 
-ContVars <- c( "NA.","NA..1",                                                    
-               "X1.1.1.New.doctorate.graduates","X1.1.2.Population.completed.tertiary.education",           
-               "X1.1.3.Lifelong.learning","X1.2.1.International.scientific.co.publications",          
-               "X1.2.2.Scientific.publications.among.top.10..most.cited","X1.2.3.Foreign.doctorate.students",                        
-               "X1.3.1.Broadband.penetration","X1.3.2.Opportunity.driven.entrepreneurship",               
-               "X2.1.1.R.D.expenditure.in.the.public.sector","X2.1.2.Venture.capital.investments",                       
-               "X2.2.1.R.D.expenditure.in.the.business.sector","X2.2.2.Non.R.D.innovation.expenditure",                    
-               "X2.2.3.Enterprises.providing.ICT.training","X3.1.1.SMEs.with.product.or.process.innovations",          
-               "X3.1.2.SMEs.with.marketing.or.organisational.innovations","X3.1.3.SMEs.innovating.in.house",                          
-               "X3.2.1.Innovative.SMEs.collaborating.with.others","X3.2.2.Public.private.co.publications",                    
-               "X3.2.3.Private.co.funding.of.public.R.D.expenditures","X3.3.1.PCT.patent.applications",                           
-               "X3.3.2.Trademark.applications","X3.3.3.Design.applications",                               
-               "X4.1.1.Employment.in.knowledge.intensive.activities","X4.1.2.Employment.fast.growing.firms.innovative.sectors",  
-               "X4.2.1.Medium...high.tech.product.exports","X4.2.2.Knowledge.intensive.services.exports",              
-               "X4.2.3.Sales.of.new.to.market.and.new.to.firm.innovations","Summary.Innovation.Index")  
+ContVars <- c( "International scientific publications"="X1.2.1.International.scientific.co.publications",          
+               "Scientific publications among top 10 most cited"="X1.2.2.Scientific.publications.among.top.10..most.cited",
+               "Broadband penetration"="X1.3.1.Broadband.penetration", #not relevant
+               "Opportunity driven entrepreneurship"="X1.3.2.Opportunity.driven.entrepreneurship",#not relevant               
+               "R&D expenditure in the public sector"="X2.1.1.R.D.expenditure.in.the.public.sector",
+               "R&D expenditure in the business sector"="X2.2.1.R.D.expenditure.in.the.business.sector",
+               "Venture capital investment"="X2.1.2.Venture.capital.investments",#not relevant                       
+               "Non R&D innovation expenditure"="X2.2.2.Non.R.D.innovation.expenditure",#not relevant                    
+               "Entreprises providing ICT training"="X2.2.3.Enterprises.providing.ICT.training",#not so relevant
+               "SMEs with product/process innovations"="X3.1.1.SMEs.with.product.or.process.innovations",          
+               "SMEs with marketing/organisational innovations"="X3.1.2.SMEs.with.marketing.or.organisational.innovations",
+               "SMEs innovating in house"="X3.1.3.SMEs.innovating.in.house",#not relevant                        
+               "SMEs collaborating with others"="X3.2.1.Innovative.SMEs.collaborating.with.others",#not so relevant
+               "Public and private pubblications"="X3.2.2.Public.private.co.publications",                    
+               "Private co-funding of public R&D expenditures"="X3.2.3.Private.co.funding.of.public.R.D.expenditures",#not relevant
+               "PCT patent applications"="X3.3.1.PCT.patent.applications", #not relevant                          
+               "Trademark applications"="X3.3.2.Trademark.applications",
+               "Design applications"="X3.3.3.Design.applications", #not relevant                              
+               "Employment in knowledge intensive activities"="X4.1.1.Employment.in.knowledge.intensive.activities",
+               "Employment fast growing firms innovative sectors"="X4.1.2.Employment.fast.growing.firms.innovative.sectors",#not relevant  
+               "Medium & high tech product exports"="X4.2.1.Medium...high.tech.product.exports",#not relevant
+               "Knowledge intensive services exports"="X4.2.2.Knowledge.intensive.services.exports",              
+               "Sales of new to market and new to firm innovations"="X4.2.3.Sales.of.new.to.market.and.new.to.firm.innovations",
+               "Summary Innovation Index"="Summary.Innovation.Index")  
 
 doctorate<-"X1.1.1.New.doctorate.graduates"
 # User Interface
 ui <- basicPage(
+  
   headerPanel("Correlations with doctorate students"),
   
   sidebarPanel ( 
-    
-    
-    # p("Results of a chemical analysis of wines
-    #   grown in the same region in Italy but from
-    #   three different cultivars."),
-    # 
-    # p("Data from the ",
-    #   a(href = "https://archive.ics.uci.edu/ml/datasets/Wine", "UCI Machine Learning Repository.")),
-    
-    selectInput('x','x-merda',doctorate),
     selectInput('y','y-axis',ContVars, selected = "X1.1.1.New.doctorate.graduates")
-    
   ),
+  
   mainPanel(plotlyOutput(outputId = "myplot"))
-  
-  
-  
 )
 
 # Server
 server <- function(input, output) {
-  
+
   output$myplot <- renderPlotly({
     ggplotly(
-      ggplot(df,aes(x=get(input$y),
-                      y=X1.1.1.New.doctorate.graduates,
-                      size= Summary.Innovation.Index, color=location, name=NA.)) +
+      ggplot(df,aes(x=X1.1.1.New.doctorate.graduates,
+                    y=get(input$y),
+                    size= Summary.Innovation.Index, 
+                    color=location, 
+                    name=nation)) +
       geom_point(alpha=0.8,stroke = 2) +
-      scale_size(range = c(0.01, 10), name="Innovation index")+
-      scale_color_manual(values = c("s" = "#984ea3", "n" = "#377eb8", "w"="#4daf4a","e"="#e41a1c")),
+      labs(x = "New doctorate graduates", y = names(ContVars[which(ContVars == input$y)]))+
+      scale_color_manual(name="Region",
+                        values=c("#984ea3","#377eb8","#4daf4a","#e41a1c"),
+                        labels=c("southern", "northern", "western", "eastern"))+
+      scale_size(range = c(0.01, 10), name="Innovation index"),
+      # scale_color_manual(values = c("#984ea3","#377eb8","#4daf4a","#e41a1c"),
+      #                    labels = c("southern", "northern", "western", "eastern"))+
+      #scale_y_continuous(labels = scales::percent),
       tooltip = "name"
-    )
+    )%>% config(displayModeBar = F) 
   })  
   
 }
