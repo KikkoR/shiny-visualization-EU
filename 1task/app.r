d3bdeb0324d617e953b644a336c57ee087820df0
ostapkharysh <kharyshostap@gmail.com>
2021-01-18 13:09:44 +0200

    Final fix adding root to the main XLSX

--- 1task/app.r
+++ 1task/app.r
@@ -1,61 +1,71 @@
 library(shiny)
 library(geojsonio)
 library(leaflet)
+library(xlsx)
 
+# Reading the sourcefile
+file<-"../EIS2019_database.xlsx"
+sheetIndex = 6
+df<-read.xlsx(file, sheetIndex, header=TRUE)
 
-setClass("num.with.commas")
-setAs("character", "num.with.commas", 
-      function(from) as.numeric(gsub(",", ".", from) ) )
-
-cont <- read.csv(file = 'task1data.csv', sep=";", 
-                 colClasses=c('character', 'num.with.commas'))
+# Selecting the data used for visualization
+cont <- subset(df, select=c("NA.","X2.2.1.R.D.expenditure.in.the.business.sector"))
+remove(df)
 colnames(cont) <- c("country", "RD")
+cont["RD"] <- round(cont["RD"])
 
-
+# Loading geojson of Europe
 raw_eu <- rgdal::readOGR("Europe.geo.json", encoding = "UTF-8")
 
+# Renaming countries
 raw_eu$admin[raw_eu$admin == "Macedonia"] <- "North Macedonia"
 cont$country[cont$country == "Czechia"] <- "Czech Republic"
 
-
+# Matching data with geojson
 eu <- raw_eu
 cont <- head(cont[order(match(cont[,1],eu$admin)),], -5)
 eu <- eu[eu$admin %in% cont$country,]
 eu$rd <- cont[cont$country %in% eu$admin,]$RD
 
-no_data_eu <- subset(raw_eu,admin %in% c('Bosnia and Herzegovina','Albania','Andorra', 'Montenegro', 'Republic of Serbia', 'Belarus', 'Moldova', 'Kosovo'))
-
-
+# Creating a countries that have no data
+no_data_eu <- subset(raw_eu,admin %in% c('Bosnia and Herzegovina',
+                                         'Albania','Andorra', 'Montenegro',
+                                         'Republic of Serbia', 'Belarus',
+                                         'Moldova', 'Kosovo'))
+# Creating a sequential color map 
 pal <- colorNumeric("Blues", eu$rd)
-# Define UI for dataset viewer app ----
+# Defining UI viewer app
 ui <- fluidPage(
   titlePanel(title=h4("What’s the distribution of Research & Development score applied in business sector in the different areas of the Europe?", align="center")),
   leafletOutput("map",  width = "100%", height = "550px")
 )
 
 
-
-# Define server logic to summarize and view selected dataset ----
+# Defining server that generates polygons for selected dataset
 server <- function(input, output) {
   
   output$map <- renderLeaflet({
-    
+    # making labels for countries with data
     labels <- sprintf(
-      "<strong>%s</strong><br/> R&D:%g <br/>",
+      "<strong>%s</strong><br/> R&D score: %g <br/>",
       eu$admin, 
       eu$rd
     ) %>% lapply(htmltools::HTML)
     
-    
+    # making labels for countries with no data
     not_prov_labels <- sprintf(
       "<strong>%s</strong><br/> R&D: No data provided <br/>",
       no_data_eu$admin
     ) %>% lapply(htmltools::HTML)
     
-    leaflet(eu) %>%
+    leaflet() %>%
+      
+      # initiating a gray background
       addProviderTiles("MapBox", options = providerTileOptions(
         id = "mapbox.light",
         accessToken = Sys.getenv('MAPBOX_ACCESS_TOKEN'))) %>%
+      
+      # adding countries which have no data
       addPolygons(data = no_data_eu, 
                   stroke = FALSE, 
                   smoothFactor = 0.3,
@@ -63,18 +73,21 @@ server <- function(input, output) {
                   fillColor = "#CECFCE",
                   label = not_prov_labels
       ) %>%
-      
+      # defining borders for countries which have no data
       addPolylines(data = no_data_eu,
                   color = "black",
                   stroke = TRUE,
                   opacity = 1, 
                   weight = 0.5) %>% 
       
-      addPolygons(stroke = FALSE, 
+      # adding countries which have data
+      addPolygons(data = eu, 
+                  stroke = FALSE, 
                   smoothFactor = 0.3,
                   fillOpacity = 1,
                   fillColor = ~pal(rd), 
-                  label = labels, #~paste0(admin, ": ", formatC(RD, big.mark = ","))
+                  label = labels,
+                  #highlighting selected countries
                   highlight = highlightOptions(
                     weight = 3,
                     fillOpacity = 0.5,
@@ -84,13 +97,15 @@ server <- function(input, output) {
                     sendToBack = TRUE)
                   ) %>%
       
-      # Add border
-      addPolylines(color = "black",
-                   stroke = TRUE,
-                   opacity = 1, 
-                   weight = 0.5) %>% 
+      # defining borders for countries which have data
+      addPolylines( data = eu, 
+                    color = "black",
+                    stroke = TRUE,
+                    opacity = 1, 
+                    weight = 0.5) %>% 
       
-      addLegend(pal = pal, values = ~rd, opacity = 1.0, title = "R&D score")
+      # creating a legend
+      addLegend(data = eu, pal = pal, values = ~rd, opacity = 1.0, title = "R&D score")
 })
   
 }
